3cd85b84a9a288d94c2fa1e960b80df6e1393236
Karen Rubio <120537042+KarenRubio@users.noreply.github.com>
2024-06-20 00:11:42 +0000

    estilos

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -33,46 +33,51 @@ export const Login= () => {
                 </div>
             </nav> */}
             <div className="wrapper">
+                <span className="bg-animate"></span>
+                <span className="bg-animate2"></span>
+
                 <div className="form-box login">
-                    <form className="login-container container wrapper" onSubmit={handleSubmit}>
                         <h2>Login</h2>
-                        {error && <div className="alert alert-danger">{error}</div>}
-                        <div className="login-form-floating form-floating mb-3 input-box">
+                        <form  onSubmit={handleSubmit}>
+                            {error && <div className="alert alert-danger">{error}</div>}
+                            <div className="input-box">
                             <input
                                 onChange={(e) => setUser({ ...user, email: e.target.value })}
                                 type="email"
-                                className="login-form-control form-control"
                                 name="email"
                                 value={user.email}
-                                id="inputEmail"
-                                placeholder="Enter Email"
+                                id="inputEmail"             
                             />
                             <label htmlFor="inputEmail">Email</label>
                             <i class="bi bi-person-fill"></i>
                         </div>
-                        <div className="login-form-floating form-floating mb-3">
+                        <div className="input-box">
                             <input
                                 onChange={(e) => setUser({ ...user, password: e.target.value })}
                                 type="password"
-                                className="login-form-control form-control"
+                                
                                 name="password"
                                 value={user.password}
                                 id="inputPassword"
-                                placeholder="Enter Password"
+                               
                             />
                             <label htmlFor="inputPassword">Password</label>
                             <i class="bi bi-lock-fill"></i>
                         </div>
-                        <button type="submit" className="login-btn btn btn-primary">Login</button>
+                        <button type="submit" className="btn">Login</button>
                         <div className="logreg-link">
                             <p>Don't have an account?
                             <Link to="/signup">
-                                Signup
+                                 Signup
                                 {/* <span className="register-link login-navbar-brand navbar-brand mb-0 h1" style={{ fontSize: "50px" }}>Signup</span> */}
                             </Link>
                             </p>
                         </div>
                     </form>
+                </div>
+                <div className="info-text login">
+                    <h2>WELCOME BACK!</h2>
+                    <p>lorem ipsum, dolor sit amet consectetur adipisicing </p>
                 </div>
             </div>
             
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { SignUp } from "../component/signup";
+import { Login } from "../component/login"
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -11,6 +12,7 @@ export const Home = () => {
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
 			< SignUp />
+			< Login />
 		</div>
 	);
 };
